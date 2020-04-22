af3d444d93e9b8606978652e21b70553bf1cae23
kanerla <kanervalaurajohanna@gmail.com>
2020-04-22 13:17:21 +0300

    Add service to module.

--- diehard-card/src/app/app.component.ts
+++ diehard-card/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RulesService } from './rules.service';
 
 @Component({
   selector: 'app-root',
   template: `<h1>Instructions</h1>`,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'diehard-card';
+  constructor(rulesService : RulesService) {
+    // do the fetch
+  }
+
+  ngOnInit(): void {
+
+  }
 }
--- diehard-card/src/app/app.module.ts
+++ diehard-card/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RulesService } from './rules.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [RulesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
