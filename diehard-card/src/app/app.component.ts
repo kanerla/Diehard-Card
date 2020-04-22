a1494a9a3f29a8e79ea6110174c0f7a4dbad08c3
kanerla <kanervalaurajohanna@gmail.com>
2020-04-22 13:52:24 +0300

    Use material design elements.

--- diehard-card/src/app/app.component.ts
+++ diehard-card/src/app/app.component.ts
@@ -3,18 +3,22 @@ import { RulesService } from './rules.service';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Instructions</h1>`,
+  template: `<h1>Instructions</h1>
+  <div>{{rules}}</div>
+  <button mat-raised-button color="primary">Play</button>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   title = 'diehard-card';
   rules = ""
 
-  constructor(rulesService : RulesService) {
-    rulesService.fetch()
+  constructor(private rulesService : RulesService) {
+
   }
 
   ngOnInit(): void {
-
+    this.rulesService.fetch((result) => {
+      this.rules = result
+    })
   }
 }
