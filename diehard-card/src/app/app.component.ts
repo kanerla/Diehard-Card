f888519bf7cf00e28321806ea9a881354a6f62e9
kanerla <kanervalaurajohanna@gmail.com>
2020-04-28 13:20:03 +0300

    Add routing.

--- diehard-card/src/app/app.component.ts
+++ diehard-card/src/app/app.component.ts
@@ -1,25 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { RulesService } from './rules.service';
 
 @Component({
   selector: 'app-root',
   template: `<h1>{{title}}</h1>
-  <div [innerHTML]="rules"></div>
-  <img src="assets/scoreboard.JPG" alt="Scoreboard"/>
-  <button mat-raised-button color="primary">Play</button>`,
+  <div><router-outlet></router-outlet></div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   title = 'Single-player Open-face Chinese Poker';
-  rules = ""
 
-  constructor(private rulesService : RulesService) {
+  constructor() {
 
   }
 
   ngOnInit(): void {
-    this.rulesService.fetch((result) => {
-      this.rules = result
-    })
+
   }
 }
--- diehard-card/src/app/app.module.ts
+++ diehard-card/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RulesService } from './rules.service';
+import { CardsService } from './cards.service';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { GameComponent } from './game/game.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+
+const appRoutes: Routes = [
+  { path: 'play', component: GameComponent },
+  { path: '', component: InstructionsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    InstructionsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
-  providers: [RulesService],
+  providers: [RulesService, CardsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
