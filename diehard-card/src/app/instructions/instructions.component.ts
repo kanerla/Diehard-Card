b63424c7d749334ff97a9fffffde7ce8ca4b9c58
kanerla <kanervalaurajohanna@gmail.com>
2020-04-28 13:18:28 +0300

    Move instructions to a separate component.

---
+++ diehard-card/src/app/instructions/instructions.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { RulesService } from '../rules.service';
+
+@Component({
+  selector: 'app-instructions',
+  template: `<div [innerHTML]="rules"></div>
+  <img src="assets/scoreboard.JPG" alt="Scoreboard"/>
+  <a routerLink="/play" routerLinkActive="active"><button mat-raised-button color="primary">Play</button></a>`,
+  styleUrls: ['./instructions.component.css']
+})
+export class InstructionsComponent implements OnInit {
+  rules = ""
+  constructor(private rulesService : RulesService) { }
+
+  ngOnInit(): void {
+    this.rulesService.fetch((result) => {
+      this.rules = result
+    })
+  }
+
+}
