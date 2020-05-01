01385e0e31c106416e995ecbb0baa8e6c078716c
kanerla <kanervalaurajohanna@gmail.com>
2020-05-01 14:59:48 +0300

    Check created poker hands.

--- diehard-card/src/app/game/game.component.ts
+++ diehard-card/src/app/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CardsService } from '../cards.service';
 import { Card } from '../card';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-game',
@@ -72,6 +73,7 @@ export class GameComponent implements OnInit {
     if (this.top.length === 3) {
       this.topFull = true
     }
+    console.log(this.gameOver())
   }
 
   placeMiddle() : void {
@@ -80,6 +82,7 @@ export class GameComponent implements OnInit {
     if (this.middle.length === 5) {
       this.middleFull = true
     }
+    console.log(this.gameOver())
   }
 
   placeBottom() : void {
@@ -88,5 +91,106 @@ export class GameComponent implements OnInit {
     if (this.bottom.length === 5) {
       this.bottomFull = true
     }
+    console.log(this.gameOver())
+  }
+
+  gameOver() : boolean {
+    if (this.bottomFull && this.topFull && this.middleFull) {
+      console.log("Game over!");
+      console.log('top:')
+      console.log(this.checkForFlush(this.top))
+      console.log('middle:')
+      console.log(this.checkForFlush(this.middle))
+      console.log('bottom:')
+      console.log(this.checkForFlush(this.bottom))
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  allEqual(arr : string[]) : boolean {
+    return arr.every(x => x === arr[0])
+  }
+
+  checkForFlush(row : Card[]) : boolean {
+    let suits = [];
+    for (let i=0; i<row.length; i++) {
+        suits.push(row[i].suit)
+    }
+    return this.allEqual( suits );
+  }
+
+  checkForStraight(row : Card[]) : boolean {
+    let sorted = this.sortValues(row)
+    if (sorted[sorted.length - 1] === 14 && sorted[0] === 2 && sorted[1] === 3 && sorted[2] === 4 && sorted[3] === 5) {
+      return true
+    }
+    for (let i = 0; i < sorted.length - 1; i++) {
+      if (sorted[i + 1] !== sorted[i] + 1) {
+        return false
+      }
+    }
+    return true
+  }
+
+  checkForThreeOfKind(row : Card[]) : boolean {
+    let sorted = this.sortValues(row)
+    let current = null;
+    let count = 0;
+
+    for (let i = 0; i < sorted.length; i++) {
+      if (sorted[i] != current) {
+          if (count === 3) {
+              console.log(current + ' comes --> ' + count + ' times');
+              return true;
+          }
+          current = sorted[i];
+          count = 1;
+      } else {
+          count++;
+      }
+    }
+    if (count === 3) {
+        console.log(current + ' comes --> ' + count + ' times');
+        return true;
+    }
+    return false
+  }
+
+  checkForPairs(row : Card[]) : void {
+    let sorted = this.sortValues(row)
+
+    let pairs = [];
+    for (let i = 0; i < sorted.length - 1; i++) {
+      if (sorted[i + 1] === sorted[i]) {
+        pairs.push(sorted[i]);
+      }
+    }
+    for (let num of pairs) {
+      console.log(num);
+    }
+    console.log(pairs.length + " pairs")
+  }
+
+  sortValues(row : Card[]) : number[] {
+    let values = [];
+    for (let i=0; i<row.length; i++) {
+      if (row[i].value === "JACK") {
+        values.push(11)
+      } else if (row[i].value === "QUEEN") {
+        values.push(12)
+      } else if (row[i].value === "KING") {
+        values.push(13)
+      } else if (row[i].value === "ACE") {
+        values.push(14)
+      } else {
+        values.push(Number(row[i].value))
+      }
+    }
+
+    let sorted = values.sort((n1, n2) => n1 - n2);
+
+    return sorted
   }
 }
