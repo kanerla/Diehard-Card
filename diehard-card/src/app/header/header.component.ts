92f03795e1b00ce47b2a8940c662b610341ab1f8
kanerla <kanervalaurajohanna@gmail.com>
2020-05-15 16:28:09 +0300

    Update the game name and rules.

--- diehard-card/src/app/header/header.component.ts
+++ diehard-card/src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title = 'Single-player Open-face Chinese Poker';
+  title = 'Simplified Single-player Open-face Chinese Poker';
 
   constructor() { }
 
